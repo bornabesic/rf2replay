6a49732d0d886e4440b29fc3270fb50a25e7d4cd
Borna Bešić <git.o8yim@aleeas.com>
2022-12-03 17:37:56 +0100

    Decompress gzipped files in a web worker

--- src/lib/FilePicker.svelte
+++ src/lib/FilePicker.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-    import { inflate } from "pako";
+    import { createEventDispatcher, getContext } from "svelte";
 
     const dispatch = createEventDispatcher();
+    const worker: Worker = getContext("worker");
 
     function onFileSelected(event: Event) {
         const input = event.target as HTMLInputElement;
         const file: File = input.files.item(0);
         file.arrayBuffer().then((data: ArrayBuffer) => {
-            let bytes = new Uint8Array(data);
+            const bytes = new Uint8Array(data);
             const isCompressed = bytes.at(0) === 0x1f && bytes.at(1) === 0x8b; // gzip magic
-            if (isCompressed) {
-                bytes = inflate(bytes);
+            if (!isCompressed) {
+                dispatch("fileLoaded", bytes);
+            } else {
+                const onDecompressedBytes = (
+                    event: MessageEvent<Uint8Array>
+                ) => {
+                    const bytesDecompressed = event.data;
+                    dispatch("fileLoaded", bytesDecompressed);
+                    worker.removeEventListener("message", onDecompressedBytes);
+                };
+
+                worker.addEventListener("message", onDecompressedBytes);
+                worker.postMessage({
+                    type: "decompress",
+                    data: bytes,
+                });
             }
-            dispatch("fileLoaded", bytes);
         });
     }
 </script>
