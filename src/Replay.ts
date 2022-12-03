0228e389bf7698e350af92b5eaa625ab67f6a49f
Borna Bešić <git.o8yim@aleeas.com>
2022-12-03 19:32:51 +0100

    Read replay info

--- src/Replay.ts
+++ src/Replay.ts
@@ -10,6 +10,7 @@ export default class Replay {
     constructor(data: Uint8Array) {
         this.data = data;
         this.readVcrHeader();
+        this.readReplayInfo();
     }
 
     private readVcrHeader() {
@@ -21,12 +22,36 @@ export default class Replay {
         this.expect(0x0A);
         this.expectString("IRSR");
         this.version = this.readFloat().toPrecision(3);
-        // TODO
+    }
+
+    private readReplayInfo() {
+        const rfm = this.readString();
+        this.skip(4); // Unknown
+        const modInfo = this.readString();
+        const scnFilename = this.readString()
+        const aiwFilename = this.readString()
+        const modName = this.readString(2)
+        const modVersion = this.readString(2)
+        const modUid = this.readString(2)
+        const trackPath = this.readString(2)
+
+        this.skip(1); // Unknown
+
+        // TODO sessionType, isPrivateSession = this.readSessionInfo()
+
+        this.skip(67); // Unknown
+    }
+
+    private skip(size: number) {
+        this.consumeIndex += size;
+        this.bufferIndex = this.consumeIndex;
     }
 
     private readStringWholeBuffer(): string {
-        const bytes: number[] = Array.from(this.data.subarray(this.bufferIndex, this.consumeIndex));
-        const string = String.fromCharCode(...bytes);
+        let string = "";
+        for (let i = this.bufferIndex; i < this.consumeIndex; ++i) {
+            string += String.fromCharCode(this.data[i]);
+        }
         this.bufferIndex = this.consumeIndex;
         return string;
     }
@@ -38,6 +63,31 @@ export default class Replay {
         const number = view.getFloat32(0, littleEndian);
         this.bufferIndex = this.consumeIndex;
         return number;
+    }
+
+    private readInteger(size: number = 4, littleEndian: boolean = true): number {
+        if (size == 3) size = 4;
+
+        this.consumeIndex += size;
+        const view = new DataView(this.data.buffer, this.bufferIndex, size);
+        this.bufferIndex = this.consumeIndex;
+        if (size == 1)
+            return view.getUint8(0);
+        else if (size == 2)
+            return view.getUint16(0, littleEndian);
+        else if (size == 4) {
+            return view.getUint32(0, littleEndian);
+        }
+        else {
+            throw Error(`Unsupported integer size: ${size}`);
+        }
+    }
+
+    private readString(lengthSize: number = 4): string {
+        const length = this.readInteger(lengthSize);
+        this.bufferIndex = this.consumeIndex;
+        this.consumeIndex += length;
+        return this.readStringWholeBuffer();
     }
 
     private expect(...bytes: number[]) {
